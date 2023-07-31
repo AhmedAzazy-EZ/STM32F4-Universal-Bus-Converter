fef3c7225853886d130f0ccf507aa273b8104bde
yahiayasser <ahmedazazyez@gmail.com>
2023-08-01 01:32:35 +0300

    CAN COM Done

--- main.cpp
+++ main.cpp
@@ -64,7 +64,7 @@ int main(void)
 	My_UART4->obsrvables_tracking.push_back(My_UART5->attatch(My_UART4 , My_UART5)); //Attach UART4 to UART5
 	My_UART5->obsrvables_tracking.push_back(My_UART4->attatch(My_UART5 , My_UART4)); //Attach UART5 to UART4 
 	My_I2C1->obsrvables_tracking.push_back(My_UART4->attatch(My_I2C1 , My_UART4));   //Attatch I2C1 to UART4
-	//My_UART4->obsrvables_tracking.push_back(My_I2C1->attatch(My_UART4 , My_I2C1));    //attach UART4 to I2C1
+	My_UART4->obsrvables_tracking.push_back(My_I2C1->attatch(My_UART4 , My_I2C1));    //attach UART4 to I2C1
 	My_UART4->obsrvables_tracking.push_back(My_CAN1->attatch(My_UART4 , My_CAN1));		//attach UART_4 to CAN1
 	
 	
