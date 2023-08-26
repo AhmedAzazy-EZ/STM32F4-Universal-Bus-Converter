6ebb3cf7e9960eb9029f0fb87cd69f190b0b2f3c
yahiayasser <ahmedazazyez@gmail.com>
2023-08-26 18:29:21 +0300

    #Yeah Boy , DONE ^_^

--- ETH_SendP.py
+++ ETH_SendP.py
@@ -5,7 +5,7 @@ src_mac = "98:43:FA:28:3C:14"
 dst_mac = "00:12:2D:2D:12:00"
 
 # Define the data payload
-data_payload = "Ahmed Azazy"
+data_payload = input("Enter Payload to be sent")
 
 # Create an Ethernet frame with the specified source and destination MAC addresses
 eth_frame = scapy.Ether(src=src_mac, dst=dst_mac)
